afc34889721c9644644eec3bb815b957a0034451
Shino Chan <40408092+MingHaoC@users.noreply.github.com>
2020-11-09 18:29:30 -0500

    Switch to token based authenication using JWT

--- src/main/java/Doable/Application.java
+++ src/main/java/Doable/Application.java
@@ -2,6 +2,12 @@ package Doable;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @SpringBootApplication
 public class Application {
@@ -9,5 +15,21 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	@EnableWebSecurity
+	@Configuration
+	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable()
+					.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+					.authorizeRequests()
+					.antMatchers(HttpMethod.POST, "/api/v1/user/register").permitAll()
+					.antMatchers(HttpMethod.GET, "/api/v1/user/login").permitAll()
+					.anyRequest().authenticated();
+		}
+	}
+
 
 }
--- src/main/java/Doable/api/CalendarController.java
+++ src/main/java/Doable/api/CalendarController.java
@@ -1,9 +1,11 @@
 package Doable.api;
 
-import org.json.JSONException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.json.JSONObject;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
@@ -12,20 +14,53 @@ import javax.validation.constraints.NotNull;
 @RestController
 public class CalendarController {
 
+    // todo: Get the token from the header
+
     @PostMapping("/create")
-    public void addEvent(@Valid @NotNull @RequestBody String EventInfo) {
+    public void addEvent(@Valid @NotNull @RequestBody String EventInfo, HttpServletRequest request) {
         JSONObject jObject = new JSONObject(EventInfo);
+        //Get token info
+        Claims body = Jwts.parser()
+                .setSigningKey("mySecretKey")
+                .parseClaimsJws(request.getHeader("Authorization"))
+                .getBody();
+        System.out.println(body.getSubject());
     }
 
     @PutMapping("/update")
-    public void updateEvent(@Valid @NotNull @RequestBody String EventInfo) {
+    public void updateEvent(@Valid @NotNull @RequestBody String EventInfo, HttpServletRequest request) {
         JSONObject jObject = new JSONObject(EventInfo);
+        /*Claims body = Jwts.parser()
+                .setSigningKey("mySecretKey")
+                .parseClaimsJws(token)
+                .getBody(); */
     }
 
     @DeleteMapping("/delete")
-    public void deleteEvent(@Valid @NotNull @RequestBody String EventInfo) {
+    public void deleteEvent(@Valid @NotNull @RequestBody String EventInfo, HttpServletRequest request) {
         JSONObject jObject = new JSONObject(EventInfo);
+
+        /*Claims body = Jwts.parser()
+                .setSigningKey("mySecretKey")
+                .parseClaimsJws(token)
+                .getBody();
+                */
     }
 
+    /**
+     * Test method used for testing http request
+     * @param request request info
+     */
+    @PostMapping("/hello")
+    public void Hello(HttpServletRequest request){
+        System.out.println("you have the token, nice!");
+        String token = request.getHeader("Authorization").replace("Bearer", "");
+        System.out.println(token);
+        Claims body = Jwts.parser()
+                .setSigningKey("mySecretKey")
+                .parseClaimsJws(token)
+                .getBody();
+        System.out.println(body.getSubject());
+    }
 
 }
